641b21baee0e550c237e08de8af1802aa69804aa
chenxiaoge <775719906@qq.com>
2018-04-25 10:31:35 +0800

    后台框架引入Springboot

---
+++ src/main/java/com/yunda/wwms/WwmsApplication.java
@@ -0,0 +1,12 @@
+package com.yunda.wwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwmsApplication.class, args);
+	}
+}
