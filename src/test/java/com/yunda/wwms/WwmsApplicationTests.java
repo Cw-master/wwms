652dc5646e170bc8ab7ff4fdccb4e35c3ed45ffa
chenxiaoge <775719906@qq.com>
2018-04-25 10:41:29 +0800

    ws-001 后台引入Springboot

---
+++ src/test/java/com/yunda/wwms/WwmsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yunda.wwms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WwmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
